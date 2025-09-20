05386431839b2e3605b0035919df2dd8c211d437
Haris444-coder <hmaleji@gmail.com>
2025-09-20 14:14:36 -0400

    Delete Week 7 directory

--- Week 7/Week 7 - Code/1/script.js
+++
@@ -1,6 +0,0 @@
-let form = document.getElementById("myForm");
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  console.log("Muhahaha I stopped the form submitting. 😈");
-});
--- Week 7/Week 7 - Code/2/script.js
+++
@@ -1,9 +0,0 @@
-let form = document.getElementById("myForm");
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  let nameField = form.elements["name"];
-  let phoneField = form.elements.phone;
-  console.log(nameField.value);
-  console.log(phoneField.value);
-});
