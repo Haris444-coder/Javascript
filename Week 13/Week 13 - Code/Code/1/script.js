0271971f27d7a3a4721eeaff589c8d6904e4b1b5
Haris444-coder <hmaleji@gmail.com>
2025-09-20 14:15:20 -0400

    Delete Week 13 directory

--- Week 13/Week 13 - Code/Code/1/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("runButton").addEventListener("click", function () {
-  console.log("Dog");
-  setTimeout(() => {
-    console.log("Cat");
-  }, 2000);
-  console.log("Bird");
-});
